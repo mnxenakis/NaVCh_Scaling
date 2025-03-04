c32b54c3d880a76100dee97db56ba78afe9da9dc
mnxenakis <167875185+mnxenakis@users.noreply.github.com>
2025-03-04 09:54:49 +0100

    Add files via upload

--- ModelParameters.py
+++ ModelParameters.py
@@ -1,45 +1,46 @@
 '''
-		Parameter list
-'''
 
+		Parameter values
+
+'''
 ## RG parameters
-N_SCALES = 800 # nr of scales (this is also the sampling resolution along the radial direction: \alpha = 1,2,..,N_SCALES)
-N_HYDR = 6 # ODD and EVEN nr of exp free energy obs (hydr mom), nr of moments is: 2*Key_Parameters.N_HYDR
+N_SCALES = 800 					# Nr of scales (this is also the sampling resolution along the radial direction: \alpha = 1,2,..,N_SCALES)
+N_HYDR = 6 						# ODD and EVEN nr of exp free energy obs (hydr mom), nr of moments is: 2*Key_Parameters.N_HYDR
 
 ## Hole parameters
-N_RUNS_HOLE = 50 # HOLE runs
-ROUND_DIGIT_PP = 2 # pore points coordinates will be rounded to ROUND_DIGIT_PP decimal digit
-HOLE_RES = 1e-1 # SAMPLE parameter of the hole.inp file
+N_RUNS_HOLE = 50 				# HOLE runs
+ROUND_DIGIT_PP = 2 				# Pore points coordinates will be rounded to ROUND_DIGIT_PP decimal digit
+HOLE_RES = 1e-1 				# SAMPLE parameter of the hole.inp file
 
 ## Additive noise parameters
-STD_NOISE = 1e-3 # *very* weak noise profile ("smooths" hydropathic profiles mimicking the effect of room temperature fluctuations)
-ZERO = 1e-04 # numerial correction for the hydropathic energy of empty initial shells
-
-## Geom model parameters
-NU_MIN = 1e-3 # smallest value of \nu (\nu \to 0)
+STD_NOISE = 1e-3 				# *Very* weak noise profile ("smooths" hydropathic profiles mimicking the effect of room temperature fluctuations)
+ZERO = 1e-04 					# Numerial zero 
 
 ## Numerical comparisons of pore point coordinates
 ACC	= 1e-2 
 
 ## Numerical differentiation and smoothing  
-POLYORDER = 2 # smoothing parameter (degree of the polynomial used to fit the data within each window)
-# window factor
-WINDOW_FAC = 1.0 # adjsuts the window size (the smaller, the more detailed the scaling picture)
+POLYORDER = 2 					# Smoothing parameter (degree of the polynomial used to fit the data within each window)
+
+## Window factor (for computing derivatives)
+WINDOW_FAC = 1 					# Adjsuts the window size (the smaller, the more detailed the scaling picture)
 
 ## Machine Learning Parameters
-# K-fold cross-validation 
-CLASSIFIER_SEED	= 42 # classifier seed	
-K = 2 # maximum nr of folds (determines size of validation test: 1/K) (note that we consider K - 1 different folds: 2, 3, .. , K) run with: 3
-NUM_OF_TRAININGS = 20 # number of k-fold trainings # run with: 20 (local learning) and 50 (ensemble learning)
-NUM_OF_IMBALANCES_REMOVAL = 10 # number of balancing attempts for the "unseen" data set
-# Log Reg parameters
-MAX_ITER = 1000	# Max Iter of Log Reg
-C = 1 # C param of Log Reg
 
-# SVC parameters
+# K-fold cross-validation 
+CLASSIFIER_SEED	= 42 			# Classifier seed	
+K = 3 							# Maximum nr of folds (determines size of validation test: 1/K) (note that we consider K - 1 different folds: 2, 3, .. , K) 
+NUM_OF_TRAININGS = 20 			# Nr of k-fold trainings # run with: 20 (local learning) and 50 (ensemble learning)
+NUM_OF_IMBALANCES_REMOVAL = 10 	# Nr of balancing attempts for the "unseen" data set
+
+# Logistic Regression classifier parameters
+MAX_ITER = 1000					
+C = 1 							
+
+# SVM classifier parameters
 KERNEL_NONLINEAR = 'rbf'
 KERNEL_LINEAR = 'linear'
-NUM_OF_PERM_REPEATS	= 10 # nr of permuations (feature importance evaluation for SVC) # run with 10
+NUM_OF_PERM_REPEATS	= 10 		# Nr of permuations (feature importance evaluation during SVM) 
 
 ## Percentiles plotting parameter
 NUM_OF_PERCENTILES = 20
